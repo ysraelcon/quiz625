a0833aebb77476cf2e17e36616eae0c5d6dcbc78
ysraelcon <ysraelcon@gmail.com>
2017-06-25 19:11:51 -0400

    sqlite, sequelize, pg, listo

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,19 @@
+var models = require('../models/models.js');
+
 // GET /quizes/question
  exports.question = function(req, res) {
-    res.render('quizes/question', {pregunta: 'Capital de Cojedes'});
+    models.Quiz.findAll().success(function(quiz) {
+     res.render('quizes/question', { pregunta: quiz[0].pregunta});
+   })
  };
  
  // GET /quizes/answer
  exports.answer = function(req, res) {
-    if (req.query.respuesta === 'San Carlos'){
-       res.render('quizes/answer', {respuesta: 'que Geografoooo!'});
-    } else {
-       res.render('quizes/answer', {respuesta: 'mal mal'});
-    }
+    models.Quiz.findAll().success(function(quiz) {
+     if (req.query.respuesta === quiz[0].respuesta) {
+       res.render('quizes/answer', { respuesta: 'Que Inteligenteeeeje' });
+     } else {
+       res.render('quizes/answer', { respuesta: 'mal mal, mapeate'});
+     }
+   })
  };
