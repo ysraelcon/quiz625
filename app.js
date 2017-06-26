8a86d41420e2f5efe7c12ce1760b20d63a51ace8
ysraelcon <ysraelcon@gmail.com>
2017-06-25 22:31:58 -0400

    concreación de preguntajas

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 //activado favicon
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
