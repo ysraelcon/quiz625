cb10fed8027fae5c4a913e1cd904c3943a3a587a
ysraelcon <ysraelcon@gmail.com>
2017-06-26 09:15:08 -0400

    opcion borrado, y no esta creando

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -65,7 +65,7 @@ exports.answer = function(req, res) {
          .then( function(){ res.redirect('/quizes')}) 
        }      // res.redirect: Redirección HTTP a lista de preguntas
      }
-   );
+   ).catch(function(error){next(error)});
  };
 
 // GET /quizes/:id/edit
@@ -92,5 +92,14 @@ exports.answer = function(req, res) {
          .then( function(){ res.redirect('/quizes');});
        }     // Redirección HTTP a lista de preguntas (URL relativo)
      }
-   );
+   ).catch(function(error){next(error)});
  };
+
+ // DELETE /quizes/:id
+ exports.destroy = function(req, res) {
+   req.quiz.destroy().then( function() {
+     res.redirect('/quizes');
+   }).catch(function(error){next(error)});
+ };
+ 
+ //  console.log("req.quiz.id: " + req.quiz.id);
