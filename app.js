71d1c48c3379ea1df3f1f7769591a0a60c1f13b5
ysraelcon <ysraelcon@gmail.com>
2017-06-26 09:00:11 -0400

    edicion de preguntajas overraida

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
+var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
@@ -26,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,8 @@ router.get('/', function(req, res) {
  router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
  router.post('/quizes/create',              quizController.create);
+ router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+ router.put('/quizes/:quizId(\\d+)',        quizController.update);
 
 
 module.exports = router;
