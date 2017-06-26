9fa047ccda82180400bf88654cdc7481fbfad1c4
ysraelcon <ysraelcon@gmail.com>
2017-06-26 10:55:52 -0400

    crecomenetario, errando en quizid indefinido

--- models/models.js
+++ models/models.js
@@ -33,8 +33,16 @@ var path = require('path');
  var quiz_path = path.join(__dirname,'quiz');
  var Quiz = sequelize.import(quiz_path);
  
- exports.Quiz = Quiz; // exportar definición de tabla Quiz
+// Importar definicion de la tabla Comment
+ var comment_path = path.join(__dirname,'comment');
+ var Comment = sequelize.import(comment_path);
  
+ Comment.belongsTo(Quiz);
+ Quiz.hasMany(Comment);
+
+ exports.Quiz = Quiz; // exportar definición de tabla Quiz
+ exports.Comment = Comment;
+
  // sequelize.sync() crea e inicializa tabla de preguntas en DB
  sequelize.sync().then(function() {
    // success(..) ejecuta el manejador una vez creada la tabla
