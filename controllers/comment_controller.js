6e1f35ae127b9a1b46afbf3afa47b7486e2fc57f
ysraelcon <ysraelcon@gmail.com>
2017-07-06 16:14:03 -0400

    casi fin, no tal column

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,6 @@
 var models = require('../models/models.js');
- 
+var Sequelize=require('sequelize'); 
+
 // Autoload :id de comentarios
  exports.load = function(req, res, next, commentId) {
    models.Comment.find({
@@ -28,18 +29,11 @@ var models = require('../models/models.js');
          });
  
    comment
-   .validate()
-   .then(
-     function(err){
-       if (err) {
-         res.render('comments/new.ejs', {comment: comment, errors: err.errors});
-       } else {
-         comment // save: guarda en DB campo texto de comment
-         .save()
-         .then( function(){ res.redirect('/quizes/'+req.params.quizId)}) 
-       }      // res.redirect: Redirección HTTP a lista de preguntas
-     }
-   ).catch(function(error){next(error)});
+   .save() 
+	 .then( function(){ res.redirect('/quizes/'+req.params.quizId)}) 
+   .catch(Sequelize.ValidationError, function (err) { 
+	      res.render('comments/new.ejs', {comment: comment, errors: err.errors}); 
+	     }).catch(function(error){next(error)});
    
  };
 
@@ -48,7 +42,7 @@ var models = require('../models/models.js');
  exports.publish = function(req, res) {
    req.comment.publicado = true;
  
-   req.comment.save( {fields: ["publicado"]})
+   req.comment.save( {fields: ['publicado']})
      .then( function(){ res.redirect('/quizes/'+req.params.quizId);} )
      .catch(function(error){next(error)});
  
